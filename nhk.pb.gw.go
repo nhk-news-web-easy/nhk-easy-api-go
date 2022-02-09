dc9a17c8d37abb98e725f9c0fee1a514bebde047
Frederick-S <mao_xiaodan@hotmail.com>
2022-02-09 21:25:33 +0800

    get words

--- nhk.pb.gw.go
+++ nhk.pb.gw.go
@@ -67,6 +67,42 @@ func local_request_NhkService_GetNews_0(ctx context.Context, marshaler runtime.M
 
 }
 
+var (
+	filter_NhkService_GetWords_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_NhkService_GetWords_0(ctx context.Context, marshaler runtime.Marshaler, client NhkServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq WordRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_NhkService_GetWords_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetWords(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_NhkService_GetWords_0(ctx context.Context, marshaler runtime.Marshaler, server NhkServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq WordRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_NhkService_GetWords_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetWords(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterNhkServiceHandlerServer registers the http handlers for service NhkService to "mux".
 // UnaryRPC     :call NhkServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -93,6 +129,29 @@ func RegisterNhkServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 		}
 
 		forward_NhkService_GetNews_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_NhkService_GetWords_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/nhk.NhkService/GetWords", runtime.WithHTTPPathPattern("/words"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_NhkService_GetWords_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_NhkService_GetWords_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -157,13 +216,37 @@ func RegisterNhkServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("GET", pattern_NhkService_GetWords_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/nhk.NhkService/GetWords", runtime.WithHTTPPathPattern("/words"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_NhkService_GetWords_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_NhkService_GetWords_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
 var (
 	pattern_NhkService_GetNews_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"news"}, ""))
+
+	pattern_NhkService_GetWords_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"words"}, ""))
 )
 
 var (
 	forward_NhkService_GetNews_0 = runtime.ForwardResponseMessage
+
+	forward_NhkService_GetWords_0 = runtime.ForwardResponseMessage
 )
--- nhk_grpc.pb.go
+++ nhk_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type NhkServiceClient interface {
 	GetNews(ctx context.Context, in *NewsRequest, opts ...grpc.CallOption) (*NewsReply, error)
+	GetWords(ctx context.Context, in *WordRequest, opts ...grpc.CallOption) (*WordReply, error)
 }
 
 type nhkServiceClient struct {
@@ -42,11 +43,21 @@ func (c *nhkServiceClient) GetNews(ctx context.Context, in *NewsRequest, opts ..
 	return out, nil
 }
 
+func (c *nhkServiceClient) GetWords(ctx context.Context, in *WordRequest, opts ...grpc.CallOption) (*WordReply, error) {
+	out := new(WordReply)
+	err := c.cc.Invoke(ctx, "/nhk.NhkService/GetWords", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // NhkServiceServer is the server API for NhkService service.
 // All implementations must embed UnimplementedNhkServiceServer
 // for forward compatibility
 type NhkServiceServer interface {
 	GetNews(context.Context, *NewsRequest) (*NewsReply, error)
+	GetWords(context.Context, *WordRequest) (*WordReply, error)
 	mustEmbedUnimplementedNhkServiceServer()
 }
 
@@ -56,6 +67,9 @@ type UnimplementedNhkServiceServer struct {
 
 func (UnimplementedNhkServiceServer) GetNews(context.Context, *NewsRequest) (*NewsReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetNews not implemented")
+}
+func (UnimplementedNhkServiceServer) GetWords(context.Context, *WordRequest) (*WordReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetWords not implemented")
 }
 func (UnimplementedNhkServiceServer) mustEmbedUnimplementedNhkServiceServer() {}
 
@@ -88,6 +102,24 @@ func _NhkService_GetNews_Handler(srv interface{}, ctx context.Context, dec func(
 	return interceptor(ctx, in, info, handler)
 }
 
+func _NhkService_GetWords_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(WordRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(NhkServiceServer).GetWords(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/nhk.NhkService/GetWords",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(NhkServiceServer).GetWords(ctx, req.(*WordRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // NhkService_ServiceDesc is the grpc.ServiceDesc for NhkService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -98,6 +130,10 @@ var NhkService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetNews",
 			Handler:    _NhkService_GetNews_Handler,
+		},
+		{
+			MethodName: "GetWords",
+			Handler:    _NhkService_GetWords_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
